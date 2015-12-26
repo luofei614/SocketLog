f6e42504479cbe9b5f9a0140ba75be39f19280d7
Zjmainstay <hzgdys@163.com>
2015-12-27 03:40:31 +0800

    id=>client_id

--- demo.php
+++ demo.php
@@ -7,7 +7,7 @@ slog(array(
     'optimize'            => false,//是否显示利于优化的参数，如果运行时间，消耗内存等，默认为false
     'show_included_files' => false,//是否显示本次程序运行加载了哪些文件，默认为false
     'error_handler'       => false,//是否接管程序错误，将程序错误显示在console中，默认为false
-    'force_client_ids'    => array(//日志强制记录到配置的client_id,默认为空,id必须在allow_client_ids中
+    'force_client_ids'    => array(//日志强制记录到配置的client_id,默认为空,client_id必须在allow_client_ids中
         //'client_01',
         //'client_02',
     ), 
